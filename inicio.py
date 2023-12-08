f5ad36a4a59973c6ac1fc4600cf53707a15ea51d
wendersonKlen <153118842+wendersonKlen@users.noreply.github.com>
2023-12-08 16:28:28 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -3,6 +3,12 @@ import streamlit as st
 st.header('PindaMobile Energy', divider='blue')
 
 horas = st.number_input("digite a quantidade de horas usando o aparelho")
+if horas <0:
+  st.write("quantidade de horas incoerente")
+
+dia = st.number_input("digite quantas vezes o mesmo é usado")
+if dia <0:
+  st.write("quantidade de horas incoerente")
                      
 
 
