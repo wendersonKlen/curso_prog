cb2cd5fb837b9cf633f196069d303f1770d1065b
wendersonKlen <153118842+wendersonKlen@users.noreply.github.com>
2023-12-08 16:32:07 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -4,7 +4,7 @@ st.header('PindaMobile Energy', divider='blue')
 
 horas = st.number_input("digite a quantidade de horas usando o aparelho")
 if horas <0:
-  st.write("quantidade de horas incoerente")
+  st.write(":red[quantidade de horas incoerente]")
 
 dia = st.number_input("digite quantas vezes no mes o mesmo é usado")
 if dia <0:
