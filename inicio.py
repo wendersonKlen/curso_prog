5c3ec5fe9bed038a350ac0c75b35ed943dac332b
wendersonKlen <153118842+wendersonKlen@users.noreply.github.com>
2023-12-08 16:22:36 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 st.header('PindaMobile Energy', divider='blue')
 
+horas = st.selectbox("quantidade de horas usando o aparelho"
+                    (number = st.number_input('Insert a number')
+
 
