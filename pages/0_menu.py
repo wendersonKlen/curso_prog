0d08f81babba8511fc14258d7a62348c053fc300
wendersonKlen <153118842+wendersonKlen@users.noreply.github.com>
2023-12-12 15:31:32 -0300

    Update 0_menu.py

--- pages/0_menu.py
+++ pages/0_menu.py
@@ -1,4 +1,9 @@
 import streamlit as st
+st.write("**atençao** este modo de calculadora usa a media dos aparelhos para fazer o calculo,
+para mais precisao usar a personalizada")
+option = st.selectbox(
+    'selecione o seu aparelho',
+    ('maquina de lavar ("faz tudo")', 'televisao', 'ar condicionado'))
 
 horas = st.number_input("digite a quantidade de horas usando o aparelho")
 if horas <0:
@@ -8,13 +13,11 @@ dia = st.number_input("digite quantas vezes no mes o mesmo é usado")
 if dia <0:
   st.write(":red[quantidade de dias incoerente]")
 
-option = st.selectbox(
-    'selecione o seu aparelho',
-    ('maquina de lavar ("faz tudo")', 'televisao', 'ar condicionado'))
-
 if option == 'maquina de lavar ("faz tudo")':
   res = (dia*horas*0.50)
-  st.write(res)
+  din = res*0,75
+  
+  st.write("o aparalho gastara R$:", din, " reais por mes.")
   
 
                      
