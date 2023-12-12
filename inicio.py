037f28d4b8b7cfef28557dc35820141bf65596d2
wendersonKlen <153118842+wendersonKlen@users.noreply.github.com>
2023-12-12 15:13:55 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.header('PindaMob Energy', divider='blue')
-st.image("https://github.com/wendersonKlen/curso_prog/blob/main/image/Capturar.PNG")
+st.image("https://github.com/wendersonKlen/curso_prog/blob/main/image/Capturar.PNG?raw=true")
 
 
 
