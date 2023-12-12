77aa7a2a102afbee234cdb792bd5337eb2483974
wendersonKlen <153118842+wendersonKlen@users.noreply.github.com>
2023-12-12 15:17:16 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -3,5 +3,19 @@ import streamlit as st
 st.header('PindaMob Energy', divider='blue')
 st.image("https://github.com/wendersonKlen/curso_prog/blob/main/image/Capturar.PNG?raw=true")
 
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors].''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
+
 
 
