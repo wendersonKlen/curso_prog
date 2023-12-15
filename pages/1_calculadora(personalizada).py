2f78e1470b68b76d84517dd81fb32aa122707dbf
wendersonKlen <153118842+wendersonKlen@users.noreply.github.com>
2023-12-15 14:28:10 -0300

    Update 1_calculadora(personalizada).py

--- pages/1_calculadora(personalizada).py
+++ pages/1_calculadora(personalizada).py
@@ -1,1 +1,7 @@
+horas = st.number_input("digite a quantidade de horas usando o aparelho")
+if horas <0:
+  st.write(":red[quantidade de horas incoerente]")
 
+dia = st.number_input("digite quantas vezes no mes o mesmo é usado")
+if dia <0:
+  st.write(":red[quantidade de dias incoerente]")
