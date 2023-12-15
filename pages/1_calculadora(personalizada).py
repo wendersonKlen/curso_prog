3066585c1852bdfa1189d7711c26d6ae0922627c
wendersonKlen <153118842+wendersonKlen@users.noreply.github.com>
2023-12-15 14:33:47 -0300

    Update 1_calculadora(personalizada).py

--- pages/1_calculadora(personalizada).py
+++ pages/1_calculadora(personalizada).py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+kwh = st.number_input("digite quantos **KWH** o seu aparelho consome ")
+if kwh <0:
+  st.write(":red[consumo incoerente]")
+  
 horas = st.number_input("digite a quantidade de horas usando o aparelho")
 if horas <0:
   st.write(":red[quantidade de horas incoerente]")
@@ -7,3 +11,7 @@ if horas <0:
 dia = st.number_input("digite quantas vezes no mes o mesmo é usado")
 if dia <0:
   st.write(":red[quantidade de dias incoerente]")
+
+valor = st.number_input("digite qual o valor da **KWH** em sua cidade ")
+if valor <0:
+  st.write(":red[valor incoerente]")
